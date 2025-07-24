ae64cea444d9fc22bebd79b9b3f6b3c36ea37b57
pavelixo <pavelixo@icloud.com>
2025-07-24 20:59:46 -0300

    initial

---
+++ src/test/java/io/github/pavelixo/relations/RelationsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pavelixo.relations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
