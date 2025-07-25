9eb10c1508b1d418f8063cbf2405208f5fc67278
pavelixo <pavelixo@icloud.com>
2025-07-24 21:20:10 -0300

    graph

---
+++ src/main/java/io/github/pavelixo/relations/model/UserDocument.java
@@ -0,0 +1,5 @@
+package io.github.pavelixo.relations.model;
+
+public class UserDocument {
+
+}
---
+++ src/main/java/io/github/pavelixo/relations/model/UserGraph.java
@@ -0,0 +1,31 @@
+package io.github.pavelixo.relations.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+import org.springframework.data.neo4j.core.schema.Relationship;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor 
+@AllArgsConstructor
+@Builder
+@Node("User")
+public class UserGraph {
+	@Id
+	private String id;
+	
+	@Builder.Default
+	@Relationship(type = "FRIENDS")
+	private Set<UserGraph> friends = new HashSet<>();
+	
+    public void addFriend(UserGraph friend) {
+        this.friends.add(friend);
+    }
+}
