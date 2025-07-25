31790a2ac43665e1798d33f0af0b32427c6c008c
pavelixo <pavelixo@icloud.com>
2025-07-24 21:23:38 -0300

    document

--- src/main/java/io/github/pavelixo/relations/model/UserDocument.java
+++ src/main/java/io/github/pavelixo/relations/model/UserDocument.java
@@ -1,5 +1,20 @@
 package io.github.pavelixo.relations.model;
 
-public class UserDocument {
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Document(collection = "users")
+public class UserDocument {
+	@Id
+	private String id;
+	private String username;
 }
